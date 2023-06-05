de4f1343aba3341461194fe0f6f452003a213ec3
temrario <vaninmixa02@gmail.com>
2023-06-05 16:52:06 +0300

    clear and sisinfo

---
+++ Terminal/Terminal/Models/TerminalRequests/clear.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Terminal.Handlers;
+using Terminal.Models.TerminalRequests.Base;
+
+namespace Terminal.Models.TerminalRequests
+{
+    internal class CLEAR_TerminalRequest : TerminalRequest
+    {
+        public CLEAR_TerminalRequest()
+        {
+            CommandName = "clear";
+        }
+
+        public override void Execute(CommandHandler handler, string commandBody = "")
+        {
+            Console.Clear();
+            base.Execute(handler, commandBody);
+        }
+    }
+}
---
+++ Terminal/Terminal/Models/TerminalRequests/sisinfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        RunCommand("systeminfo");
+    }
+
+    public static void RunCommand(string command)
+    {
+        ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe", "/c " + command)
+        {
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        Process process = new Process
+        {
+            StartInfo = processStartInfo
+        };
+
+        process.Start();
+
+        while (!process.StandardOutput.EndOfStream)
+        {
+            string output = process.StandardOutput.ReadLine();
+            Console.WriteLine(output);
+        }
+    }
+}
